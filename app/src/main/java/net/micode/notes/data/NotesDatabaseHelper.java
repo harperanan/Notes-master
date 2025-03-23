adf40a2d7d628afb6893f5cd1881af2d136c5e18
lilia <3509124655@qq.com>
2025-03-23 22:19:01 +0800

    注释NotesDatabaseHelper.java

--- app/src/main/java/net/micode/notes/data/NotesDatabaseHelper.java
+++ app/src/main/java/net/micode/notes/data/NotesDatabaseHelper.java
@@ -26,64 +26,73 @@ import net.micode.notes.data.Notes.DataColumns;
 import net.micode.notes.data.Notes.DataConstants;
 import net.micode.notes.data.Notes.NoteColumns;
 
-
+/**
+ * 用于操作笔记数据的数据库帮助类
+ * 负责数据库的创建、版本升级和相关触发器的管理
+ * 使用了Android中的SQLite数据库来存储笔记和笔记内容的信息。
+*/
 public class NotesDatabaseHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME = "note.db";
+    // 数据库基本信息，继承自 SQLiteOpenHelper 类，
+    // 提供了创建和管理 SQLite 数据库的方法
+    private static final String DB_NAME = "note.db";  // 数据库名称
 
-    private static final int DB_VERSION = 4;
-
+    private static final int DB_VERSION = 4;  // 当前数据库版本
+    //表名定义
     public interface TABLE {
         public static final String NOTE = "note";
 
         public static final String DATA = "data";
     }
 
-    private static final String TAG = "NotesDatabaseHelper";
+    private static final String TAG = "NotesDatabaseHelper"; // 日志标签
 
-    private static NotesDatabaseHelper mInstance;
-
+    private static NotesDatabaseHelper mInstance; // 单例实例
+    // 创建note表
     private static final String CREATE_NOTE_TABLE_SQL =
         "CREATE TABLE " + TABLE.NOTE + "(" +
-            NoteColumns.ID + " INTEGER PRIMARY KEY," +
-            NoteColumns.PARENT_ID + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.ALERTED_DATE + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.BG_COLOR_ID + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.CREATED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," +
-            NoteColumns.HAS_ATTACHMENT + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.MODIFIED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," +
-            NoteColumns.NOTES_COUNT + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.SNIPPET + " TEXT NOT NULL DEFAULT ''," +
-            NoteColumns.TYPE + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.WIDGET_ID + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.WIDGET_TYPE + " INTEGER NOT NULL DEFAULT -1," +
-            NoteColumns.SYNC_ID + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.LOCAL_MODIFIED + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.ORIGIN_PARENT_ID + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.GTASK_ID + " TEXT NOT NULL DEFAULT ''," +
-            NoteColumns.VERSION + " INTEGER NOT NULL DEFAULT 0" +
+            NoteColumns.ID + " INTEGER PRIMARY KEY," +  //笔记id
+            NoteColumns.PARENT_ID + " INTEGER NOT NULL DEFAULT 0," +  // 父文件夹ID
+            NoteColumns.ALERTED_DATE + " INTEGER NOT NULL DEFAULT 0," + //提醒时间
+            NoteColumns.BG_COLOR_ID + " INTEGER NOT NULL DEFAULT 0," + //背景颜色
+            NoteColumns.CREATED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," + //创建时间
+            NoteColumns.HAS_ATTACHMENT + " INTEGER NOT NULL DEFAULT 0," + //是否有附件
+            NoteColumns.MODIFIED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," + // 修改时间
+            NoteColumns.NOTES_COUNT + " INTEGER NOT NULL DEFAULT 0," + // 笔记数量（用于文件夹）
+            NoteColumns.SNIPPET + " TEXT NOT NULL DEFAULT ''," + // 内容摘要
+            NoteColumns.TYPE + " INTEGER NOT NULL DEFAULT 0," + // 类型（笔记/文件夹/系统文件夹）
+            NoteColumns.WIDGET_ID + " INTEGER NOT NULL DEFAULT 0," + // 关联小部件ID
+            NoteColumns.WIDGET_TYPE + " INTEGER NOT NULL DEFAULT -1," + // 小部件类型
+            NoteColumns.SYNC_ID + " INTEGER NOT NULL DEFAULT 0," + // 同步ID
+            NoteColumns.LOCAL_MODIFIED + " INTEGER NOT NULL DEFAULT 0," + // 本地修改标记
+            NoteColumns.ORIGIN_PARENT_ID + " INTEGER NOT NULL DEFAULT 0," + // 原始父文件夹ID
+            NoteColumns.GTASK_ID + " TEXT NOT NULL DEFAULT ''," + // 任务ID
+            NoteColumns.VERSION + " INTEGER NOT NULL DEFAULT 0" + // 版本号
         ")";
 
+    //创建data表
     private static final String CREATE_DATA_TABLE_SQL =
         "CREATE TABLE " + TABLE.DATA + "(" +
-            DataColumns.ID + " INTEGER PRIMARY KEY," +
-            DataColumns.MIME_TYPE + " TEXT NOT NULL," +
-            DataColumns.NOTE_ID + " INTEGER NOT NULL DEFAULT 0," +
-            NoteColumns.CREATED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," +
-            NoteColumns.MODIFIED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," +
-            DataColumns.CONTENT + " TEXT NOT NULL DEFAULT ''," +
-            DataColumns.DATA1 + " INTEGER," +
+            DataColumns.ID + " INTEGER PRIMARY KEY," + //id
+            DataColumns.MIME_TYPE + " TEXT NOT NULL," + // MIME类型
+            DataColumns.NOTE_ID + " INTEGER NOT NULL DEFAULT 0," + // 关联的笔记ID
+            NoteColumns.CREATED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," + // 创建时间
+            NoteColumns.MODIFIED_DATE + " INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000)," + // 修改时间
+            DataColumns.CONTENT + " TEXT NOT NULL DEFAULT ''," + // 内容
+            DataColumns.DATA1 + " INTEGER," + //以下均为扩展字段
             DataColumns.DATA2 + " INTEGER," +
             DataColumns.DATA3 + " TEXT NOT NULL DEFAULT ''," +
             DataColumns.DATA4 + " TEXT NOT NULL DEFAULT ''," +
             DataColumns.DATA5 + " TEXT NOT NULL DEFAULT ''" +
         ")";
 
+    //各类数据库触发器的定义
     private static final String CREATE_DATA_NOTE_ID_INDEX_SQL =
         "CREATE INDEX IF NOT EXISTS note_id_index ON " +
         TABLE.DATA + "(" + DataColumns.NOTE_ID + ");";
 
     /**
      * Increase folder's note count when move note to the folder
+     * 更新笔记时，其父文件夹时更新文件的计数
      */
     private static final String NOTE_INCREASE_FOLDER_COUNT_ON_UPDATE_TRIGGER =
         "CREATE TRIGGER increase_folder_count_on_update "+
@@ -96,6 +105,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Decrease folder's note count when move note from folder
+     * 移除笔记时，其父文件夹时减少文件的计数
      */
     private static final String NOTE_DECREASE_FOLDER_COUNT_ON_UPDATE_TRIGGER =
         "CREATE TRIGGER decrease_folder_count_on_update " +
@@ -109,6 +119,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Increase folder's note count when insert new note to the folder
+     * 插入新笔记时增加父文件夹计数
      */
     private static final String NOTE_INCREASE_FOLDER_COUNT_ON_INSERT_TRIGGER =
         "CREATE TRIGGER increase_folder_count_on_insert " +
@@ -121,6 +132,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Decrease folder's note count when delete note from the folder
+     *  删除笔记时减少父文件夹计数
      */
     private static final String NOTE_DECREASE_FOLDER_COUNT_ON_DELETE_TRIGGER =
         "CREATE TRIGGER decrease_folder_count_on_delete " +
@@ -132,8 +144,10 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         "  AND " + NoteColumns.NOTES_COUNT + ">0;" +
         " END";
 
+    //数据表相关触发器
     /**
      * Update note's content when insert data with type {@link DataConstants#NOTE}
+     * 当插入数据类型为 DataConstants.NOTE 时，更新笔记的内容。
      */
     private static final String DATA_UPDATE_NOTE_CONTENT_ON_INSERT_TRIGGER =
         "CREATE TRIGGER update_note_content_on_insert " +
@@ -147,6 +161,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Update note's content when data with {@link DataConstants#NOTE} type has changed
+     *当数据类型为 DataConstants.NOTE 的数据被修改时，更新笔记的内容。
      */
     private static final String DATA_UPDATE_NOTE_CONTENT_ON_UPDATE_TRIGGER =
         "CREATE TRIGGER update_note_content_on_update " +
@@ -160,6 +175,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Update note's content when data with {@link DataConstants#NOTE} type has deleted
+     * 当数据类型为 DataConstants.NOTE 的数据被删除时，清空笔记的摘要
      */
     private static final String DATA_UPDATE_NOTE_CONTENT_ON_DELETE_TRIGGER =
         "CREATE TRIGGER update_note_content_on_delete " +
@@ -173,6 +189,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Delete datas belong to note which has been deleted
+     *当笔记被删除时，删除与该笔记相关的所有数据
      */
     private static final String NOTE_DELETE_DATA_ON_DELETE_TRIGGER =
         "CREATE TRIGGER delete_data_on_delete " +
@@ -184,6 +201,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Delete notes belong to folder which has been deleted
+     *当文件夹被删除时，删除该文件夹中的所有笔记
      */
     private static final String FOLDER_DELETE_NOTES_ON_DELETE_TRIGGER =
         "CREATE TRIGGER folder_delete_notes_on_delete " +
@@ -195,6 +213,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Move notes belong to folder which has been moved to trash folder
+     *当文件夹被移动到回收站时，将该文件夹中的所有笔记也移动到回收站
      */
     private static final String FOLDER_MOVE_NOTES_ON_TRASH_TRIGGER =
         "CREATE TRIGGER folder_move_notes_on_trash " +
@@ -210,13 +229,14 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
+    //用于创建数据库
     public void createNoteTable(SQLiteDatabase db) {
         db.execSQL(CREATE_NOTE_TABLE_SQL);
         reCreateNoteTableTriggers(db);
         createSystemFolder(db);
         Log.d(TAG, "note table has been created");
     }
-
+    //用于创建数据库的触发器,在 onCreate 方法中调用
     private void reCreateNoteTableTriggers(SQLiteDatabase db) {
         db.execSQL("DROP TRIGGER IF EXISTS increase_folder_count_on_update");
         db.execSQL("DROP TRIGGER IF EXISTS decrease_folder_count_on_update");
@@ -235,11 +255,17 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(FOLDER_MOVE_NOTES_ON_TRASH_TRIGGER);
     }
 
+    /**
+     * 创建系统文件夹,分为4类
+     * 每个文件夹都有一个唯一的ID，可以通过ID在数据库中找到它们。
+     * 这些系统文件夹被用来管理笔记的分类、移动和删除等操作。
+     */
     private void createSystemFolder(SQLiteDatabase db) {
         ContentValues values = new ContentValues();
 
         /**
          * call record foler for call notes
+         * 通话记录文件夹
          */
         values.put(NoteColumns.ID, Notes.ID_CALL_RECORD_FOLDER);
         values.put(NoteColumns.TYPE, Notes.TYPE_SYSTEM);
@@ -247,6 +273,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
         /**
          * root folder which is default folder
+         * 根文件夹
          */
         values.clear();
         values.put(NoteColumns.ID, Notes.ID_ROOT_FOLDER);
@@ -255,6 +282,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
         /**
          * temporary folder which is used for moving note
+         * 临时文件夹
          */
         values.clear();
         values.put(NoteColumns.ID, Notes.ID_TEMPARAY_FOLDER);
@@ -263,13 +291,14 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
 
         /**
          * create trash folder
+         * 回收站文件夹
          */
         values.clear();
         values.put(NoteColumns.ID, Notes.ID_TRASH_FOLER);
         values.put(NoteColumns.TYPE, Notes.TYPE_SYSTEM);
         db.insert(TABLE.NOTE, null, values);
     }
-
+    //用于创建数据库的表格
     public void createDataTable(SQLiteDatabase db) {
         db.execSQL(CREATE_DATA_TABLE_SQL);
         reCreateDataTableTriggers(db);
@@ -277,6 +306,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         Log.d(TAG, "data table has been created");
     }
 
+    //用于创建数据库的触发器,在 onCreate 方法中调用
     private void reCreateDataTableTriggers(SQLiteDatabase db) {
         db.execSQL("DROP TRIGGER IF EXISTS update_note_content_on_insert");
         db.execSQL("DROP TRIGGER IF EXISTS update_note_content_on_update");
@@ -287,6 +317,12 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(DATA_UPDATE_NOTE_CONTENT_ON_DELETE_TRIGGER);
     }
 
+    /**
+     * NotesDatabaseHelper 类使用 synchronized 关键字
+     * 确保在多线程环境下只创建一个 NotesDatabaseHelper 实例。
+     * 这样可以避免并发访问数据库时出现问题。
+     */
+
     static synchronized NotesDatabaseHelper getInstance(Context context) {
         if (mInstance == null) {
             mInstance = new NotesDatabaseHelper(context);
@@ -294,12 +330,13 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         return mInstance;
     }
 
+    //在数据库首次创建时调用,依次调用 createTables() 和 createTriggers() 方法
     @Override
     public void onCreate(SQLiteDatabase db) {
         createNoteTable(db);
         createDataTable(db);
     }
-
+    //在数据库版本升级时调用。根据旧版本和新版本的不同，可以实现相关的升级逻辑。
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         boolean reCreateTriggers = false;
@@ -333,6 +370,9 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+
+    //使用 SQLiteDatabase 类提供的 execSQL() 方法
+    // 执行创建系统文件夹、表格和触发器的 SQL 语句
     private void upgradeToV2(SQLiteDatabase db) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE.NOTE);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE.DATA);
@@ -341,14 +381,14 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
     }
 
     private void upgradeToV3(SQLiteDatabase db) {
-        // drop unused triggers
+        // drop unused triggers---删除不使用的
         db.execSQL("DROP TRIGGER IF EXISTS update_note_modified_date_on_insert");
         db.execSQL("DROP TRIGGER IF EXISTS update_note_modified_date_on_delete");
         db.execSQL("DROP TRIGGER IF EXISTS update_note_modified_date_on_update");
-        // add a column for gtask id
+        // add a column for gtask id---添加gtask id
         db.execSQL("ALTER TABLE " + TABLE.NOTE + " ADD COLUMN " + NoteColumns.GTASK_ID
                 + " TEXT NOT NULL DEFAULT ''");
-        // add a trash system folder
+        // add a trash system folder---创建回收站系统文件
         ContentValues values = new ContentValues();
         values.put(NoteColumns.ID, Notes.ID_TRASH_FOLER);
         values.put(NoteColumns.TYPE, Notes.TYPE_SYSTEM);
@@ -356,6 +396,7 @@ public class NotesDatabaseHelper extends SQLiteOpenHelper {
     }
 
     private void upgradeToV4(SQLiteDatabase db) {
+        // 添加版本号字段
         db.execSQL("ALTER TABLE " + TABLE.NOTE + " ADD COLUMN " + NoteColumns.VERSION
                 + " INTEGER NOT NULL DEFAULT 0");
     }
