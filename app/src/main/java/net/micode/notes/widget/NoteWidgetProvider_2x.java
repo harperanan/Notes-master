cfe3cdf0b1be5cb2897a78537a963781ad715f1a
lilia <3509124655@qq.com>
2025-03-24 16:24:30 +0800

    Notewidgetprovider_2x.java文件添加注释

--- app/src/main/java/net/micode/notes/widget/NoteWidgetProvider_2x.java
+++ app/src/main/java/net/micode/notes/widget/NoteWidgetProvider_2x.java
@@ -23,23 +23,40 @@ import net.micode.notes.R;
 import net.micode.notes.data.Notes;
 import net.micode.notes.tool.ResourceParser;
 
+/**
+ * 定义了一个具体的笔记小部件实现类 NoteWidgetProvider_2x。
+ * 继承自抽象基类 NoteWidgetProvider，并实现了其抽象方法。
+ * 专门管理2x类型的小部件
+ */
 
 public class NoteWidgetProvider_2x extends NoteWidgetProvider {
+
+    //覆盖了 AppWidgetProvider 的 onUpdate 方法。
+    //当小部件需要更新时调用此方法（例如首次添加小部件或系统触发更新）。
+    //调用了父类的 update 方法来执行具体的小部件更新逻辑。
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.update(context, appWidgetManager, appWidgetIds);
     }
 
+    //实现了父类的抽象方法 getLayoutId。
+    //返回小部件的布局资源 ID，指定为 R.layout.widget_2x。
+    //这个布局文件定义了小部件的 UI 结构，适用于 2x 小部件类型。
     @Override
     protected int getLayoutId() {
         return R.layout.widget_2x;
     }
 
+    //实现了父类的抽象方法 getBgResourceId。
+    //根据传入的背景颜色，返回对应的资源 ID,用于获取 2x 小部件的背景资源。
     @Override
     protected int getBgResourceId(int bgId) {
         return ResourceParser.WidgetBgResources.getWidget2xBgResource(bgId);
     }
 
+    //实现了父类的抽象方法 getWidgetType。
+    //返回当前小部件的类型，指定为 Notes.TYPE_WIDGET_2X。
+    //这个值通常用于区分不同类型的笔记小部件1x、2x、4x 等
     @Override
     protected int getWidgetType() {
         return Notes.TYPE_WIDGET_2X;
