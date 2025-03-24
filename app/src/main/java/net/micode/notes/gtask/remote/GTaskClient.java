9b7042440fb1b680d69f810bee4d052697591e45
harperanan <924274871@qq.com>
2025-03-24 15:52:52 +0800

    GTaskClient.java文件添加注释

--- app/src/main/java/net/micode/notes/gtask/remote/GTaskClient.java
+++ app/src/main/java/net/micode/notes/gtask/remote/GTaskClient.java
@@ -60,36 +60,38 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
 
-
+//实现GTASK的登陆操作，进行GTASK任务的创建，创建任务列表，从网络上获取任务和任务列表的内容
+//主要使用类或技术：accountManager、JSONObject、HttoParams、authToken、Gid
 public class GTaskClient {
-    private static final String TAG = GTaskClient.class.getSimpleName();
+    private static final String TAG = GTaskClient.class.getSimpleName();//日志标签
 
-    private static final String GTASK_URL = "https://mail.google.com/tasks/";
+    private static final String GTASK_URL = "https://mail.google.com/tasks/";//GTASK基础URL
 
-    private static final String GTASK_GET_URL = "https://mail.google.com/tasks/ig";
+    private static final String GTASK_GET_URL = "https://mail.google.com/tasks/ig";//获取任务的URL
 
-    private static final String GTASK_POST_URL = "https://mail.google.com/tasks/r/ig";
+    private static final String GTASK_POST_URL = "https://mail.google.com/tasks/r/ig";//提交任务的URL
 
-    private static GTaskClient mInstance = null;
+    private static GTaskClient mInstance = null;//单例实例
 
-    private DefaultHttpClient mHttpClient;
+    private DefaultHttpClient mHttpClient;//HTTP客户端
 
-    private String mGetUrl;
+    private String mGetUrl;//获取任务的URL
 
-    private String mPostUrl;
+    private String mPostUrl;//提交任务的URL
 
-    private long mClientVersion;
+    private long mClientVersion;//客户端版本
 
-    private boolean mLoggedin;
+    private boolean mLoggedin;//登录状态
 
-    private long mLastLoginTime;
+    private long mLastLoginTime;//上次登录时间
 
-    private int mActionId;
+    private int mActionId;//操作ID
 
-    private Account mAccount;
+    private Account mAccount;//当前账号
 
-    private JSONArray mUpdateArray;
+    private JSONArray mUpdateArray;//更新任务列表的JSON数组
 
+    //初始化变量
     private GTaskClient() {
         mHttpClient = null;
         mGetUrl = GTASK_GET_URL;
@@ -102,6 +104,7 @@ public class GTaskClient {
         mUpdateArray = null;
     }
 
+    //获取实例化对象
     public static synchronized GTaskClient getInstance() {
         if (mInstance == null) {
             mInstance = new GTaskClient();
@@ -109,48 +112,57 @@ public class GTaskClient {
         return mInstance;
     }
 
+    //实现登陆操作
+    //设置登录操作限制时间，如果超时则重新登录
+    //两种登录方式：1.使用自己的URL登录；2.使用谷歌官方的URL登录
+    //返回true或者false表示登录结果
     public boolean login(Activity activity) {
         // we suppose that the cookie would expire after 5 minutes
         // then we need to re-login
-        final long interval = 1000 * 60 * 5;
+        final long interval = 1000 * 60 * 5;//判断登录操作是否超过5分钟
         if (mLastLoginTime + interval < System.currentTimeMillis()) {
             mLoggedin = false;
         }
-
+        //重新登录
         // need to re-login after account switch
         if (mLoggedin
                 && !TextUtils.equals(getSyncAccount().name, NotesPreferenceActivity
                         .getSyncAccountName(activity))) {
             mLoggedin = false;
         }
-
+        //未超时则不需要重新登录
         if (mLoggedin) {
             Log.d(TAG, "already logged in");
             return true;
         }
 
+        //更新最近一次的登录时间，改为系统当前的时间
         mLastLoginTime = System.currentTimeMillis();
+        //判断是否登录到谷歌账户
         String authToken = loginGoogleAccount(activity, false);
         if (authToken == null) {
             Log.e(TAG, "login google account failed");
             return false;
         }
 
+        //使用用户自己的域名登录
         // login with custom domain if necessary
+        //将用户账户名改为统一格式后判断是否为google账户地址
         if (!(mAccount.name.toLowerCase().endsWith("gmail.com") || mAccount.name.toLowerCase()
                 .endsWith("googlemail.com"))) {
             StringBuilder url = new StringBuilder(GTASK_URL).append("a/");
             int index = mAccount.name.indexOf('@') + 1;
             String suffix = mAccount.name.substring(index);
             url.append(suffix + "/");
-            mGetUrl = url.toString() + "ig";
-            mPostUrl = url.toString() + "r/ig";
+            mGetUrl = url.toString() + "ig";//设置用户对应的getUrl
+            mPostUrl = url.toString() + "r/ig";//设置用户对应的postUrl
 
             if (tryToLoginGtask(activity, authToken)) {
                 mLoggedin = true;
             }
         }
 
+        //如果用户账户无法登录，使用谷歌官方的URL进行登录
         // try to login with google official url
         if (!mLoggedin) {
             mGetUrl = GTASK_GET_URL;
@@ -164,9 +176,12 @@ public class GTaskClient {
         return true;
     }
 
+    //登录谷歌账户的办法
+    //1.使用令牌机制；2.使用AccontManager管理注册账号；3.返回值是账号的令牌
     private String loginGoogleAccount(Activity activity, boolean invalidateToken) {
-        String authToken;
-        AccountManager accountManager = AccountManager.get(activity);
+        String authToken;//令牌，登录操作保证安全性
+        AccountManager accountManager = AccountManager.get(activity);//AccountManager给用户提供了集中注册账号的接口
+        //获取全部以com.google结尾的账号
         Account[] accounts = accountManager.getAccountsByType("com.google");
 
         if (accounts.length == 0) {
@@ -176,6 +191,7 @@ public class GTaskClient {
 
         String accountName = NotesPreferenceActivity.getSyncAccountName(activity);
         Account account = null;
+        //遍历获得的账号信息，寻找已经记录过的账户信息
         for (Account a : accounts) {
             if (a.name.equals(accountName)) {
                 account = a;
@@ -189,12 +205,14 @@ public class GTaskClient {
             return null;
         }
 
+        //获取选中账号的令牌
         // get the token now
         AccountManagerFuture<Bundle> accountManagerFuture = accountManager.getAuthToken(account,
                 "goanna_mobile", null, activity, null, null);
         try {
             Bundle authTokenBundle = accountManagerFuture.getResult();
             authToken = authTokenBundle.getString(AccountManager.KEY_AUTHTOKEN);
+            //如果是invalidateToken，需要调用invalidateAuthToken()方法废除这个无效token
             if (invalidateToken) {
                 accountManager.invalidateAuthToken("com.google", authToken);
                 loginGoogleAccount(activity, false);
@@ -207,10 +225,12 @@ public class GTaskClient {
         return authToken;
     }
 
+    //尝试登录GTASK，只是预先判断令牌是否有效以及能否登上GTASK的方法，而不是具体实现登录的方法
     private boolean tryToLoginGtask(Activity activity, String authToken) {
         if (!loginGtask(authToken)) {
             // maybe the auth token is out of date, now let's invalidate the
             // token and try again
+            //删除过一个无效的authToken，申请一个新的后重新尝试登录
             authToken = loginGoogleAccount(activity, true);
             if (authToken == null) {
                 Log.e(TAG, "login google account failed");
@@ -225,24 +245,27 @@ public class GTaskClient {
         return true;
     }
 
+    //实现登录GTASK的具体操作
     private boolean loginGtask(String authToken) {
         int timeoutConnection = 10000;
-        int timeoutSocket = 15000;
+        int timeoutSocket = 15000;//socket是一种通信连接实现数据的交换的端口
         HttpParams httpParameters = new BasicHttpParams();
         HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
         HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
         mHttpClient = new DefaultHttpClient(httpParameters);
+        //设置本地cookie
         BasicCookieStore localBasicCookieStore = new BasicCookieStore();
         mHttpClient.setCookieStore(localBasicCookieStore);
         HttpProtocolParams.setUseExpectContinue(mHttpClient.getParams(), false);
 
         // login gtask
         try {
-            String loginUrl = mGetUrl + "?auth=" + authToken;
-            HttpGet httpGet = new HttpGet(loginUrl);
+            String loginUrl = mGetUrl + "?auth=" + authToken;//设置登录的url
+            HttpGet httpGet = new HttpGet(loginUrl);//通过登录的uri实例化网页上资源的查找
             HttpResponse response = null;
             response = mHttpClient.execute(httpGet);
 
+            //获取CookieStore里存放的cookie,看如果存有“GTL”，则说明有验证成功的有效的cookie
             // get the cookie now
             List<Cookie> cookies = mHttpClient.getCookieStore().getCookies();
             boolean hasAuthCookie = false;
@@ -255,6 +278,7 @@ public class GTaskClient {
                 Log.w(TAG, "it seems that there is no auth cookie");
             }
 
+            //获取client的内容，具体操作是在返回的Content中截取从_setup(开始到)}</script>中间的字符串内容，也就是gtask_url的内容
             // get the client version
             String resString = getResponseContent(response.getEntity());
             String jsBegin = "_setup(";
@@ -284,6 +308,7 @@ public class GTaskClient {
         return mActionId++;
     }
 
+    //创建一个用于向网络传输数据的对象
     private HttpPost createHttpPost() {
         HttpPost httpPost = new HttpPost(mPostUrl);
         httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
@@ -291,24 +316,27 @@ public class GTaskClient {
         return httpPost;
     }
 
+    //通过URL获取响应后返回的数据，也就是网络上的数据和资源
     private String getResponseContent(HttpEntity entity) throws IOException {
         String contentEncoding = null;
+        //通过URL得到HttpEntity对象，如果不为空则使用getContent()方法创建一个流将数据从网络都过来
         if (entity.getContentEncoding() != null) {
             contentEncoding = entity.getContentEncoding().getValue();
             Log.d(TAG, "encoding: " + contentEncoding);
         }
 
         InputStream input = entity.getContent();
+        //GZIP是使用DEFLATE进行压缩数据的另一个压缩库
         if (contentEncoding != null && contentEncoding.equalsIgnoreCase("gzip")) {
             input = new GZIPInputStream(entity.getContent());
         } else if (contentEncoding != null && contentEncoding.equalsIgnoreCase("deflate")) {
-            Inflater inflater = new Inflater(true);
+            Inflater inflater = new Inflater(true);//DEFLATE是一个无专利的压缩算法，可以实现无损数据压缩
             input = new InflaterInputStream(entity.getContent(), inflater);
         }
 
         try {
             InputStreamReader isr = new InputStreamReader(input);
-            BufferedReader br = new BufferedReader(isr);
+            BufferedReader br = new BufferedReader(isr);//是一个包装类，它可以包装字符流，将字符流放入缓存里，先把字符读到缓存里，到缓存满了时候，再读入内存，是为了提供读的效率而设计的
             StringBuilder sb = new StringBuilder();
 
             while (true) {
@@ -323,6 +351,7 @@ public class GTaskClient {
         }
     }
 
+    //通过JSON发送请求
     private JSONObject postRequest(JSONObject js) throws NetworkFailureException {
         if (!mLoggedin) {
             Log.e(TAG, "please login first");
