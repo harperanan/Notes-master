070982cc54d2839d55afec689ed9759e220a3a2a
stay1210 <1077667657@qq.com>
2025-03-24 14:31:12 +0800

    Node.java文件添加注释

--- app/src/main/java/net/micode/notes/gtask/data/Node.java
+++ app/src/main/java/net/micode/notes/gtask/data/Node.java
@@ -19,34 +19,39 @@ package net.micode.notes.gtask.data;
 import android.database.Cursor;
 
 import org.json.JSONObject;
+/**
+        * 应该是同步操作的基础数据类型，定义了相关指示同步操作的常量
+ * 关键字：abstract
+ */
 
 public abstract class Node {
+    //定义了各种用于表征同步状态的常量
     public static final int SYNC_ACTION_NONE = 0;
-
+    // 本地和云端都无可更新内容（即本地和云端内容一致）
     public static final int SYNC_ACTION_ADD_REMOTE = 1;
-
+// 需要在远程云端增加内容
     public static final int SYNC_ACTION_ADD_LOCAL = 2;
-
+    // 需要在本地增加内容
     public static final int SYNC_ACTION_DEL_REMOTE = 3;
-
+    // 需要在远程云端删除内容
     public static final int SYNC_ACTION_DEL_LOCAL = 4;
-
+    // 需要在本地删除内容
     public static final int SYNC_ACTION_UPDATE_REMOTE = 5;
-
+    // 需要将本地内容更新到远程云端
     public static final int SYNC_ACTION_UPDATE_LOCAL = 6;
-
+    // 需要将远程云端内容更新到本地
     public static final int SYNC_ACTION_UPDATE_CONFLICT = 7;
-
+// 同步出现冲突
     public static final int SYNC_ACTION_ERROR = 8;
-
+    // 同步出现错误
     private String mGid;
 
     private String mName;
 
     private long mLastModified;
-
+    //记录最后一次修改时间
     private boolean mDeleted;
-
+    //表征是否被删除
     public Node() {
         mGid = null;
         mName = "";
